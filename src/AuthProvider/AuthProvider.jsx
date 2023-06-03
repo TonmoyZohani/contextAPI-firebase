b3e6e22f77425642da1c63dea1af9654de62da18
TonmoyZohani <tonmoyzohani@gmail.com>
2023-06-03 23:57:56 +0600

    auth state changed

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,28 +1,48 @@
-import React, { createContext, useState } from "react";
-import app from "../firebase/firebase.config"
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const [user,setUser]=useState(null);
+  const [user, setUser] = useState(null);
 
-  const createUser=(email,password)=>{
-    return createUserWithEmailAndPassword(auth, email, password)
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut=()=>{
+    return signOut(auth);
   }
 
-  const signUser=(email,password)=>{
-    return signInWithEmailAndPassword(auth,email,password);
-  }
+  // observe auth state change
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log('Auth State Change', currentUser);
+      setUser(currentUser);
+    });
 
-const authInfo={
-  user,
-  createUser,
-  signUser
-}
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    createUser,
+    signUser,
+    logOut
+  };
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
 
   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { signOut } from "firebase/auth"; // Import the signOut function
 
 const Header = () => {
+  const { user,logOut } = useContext(AuthContext);
+
+  // Function to handle sign-out
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        // Optional: Add any additional logic after sign-out
+      })
+      .catch((error) => {
+        console.log("Error signing out:", error);
+      });
+  };
+
   return (
     <div className="bg-gray-900 py-4">
       <nav className="container mx-auto px-4">
         <ul className="flex items-center space-x-4">
-        <li>
-            <Link to="/" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
-             Home
+          <li>
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-white transition duration-300 ease-in-out"
+            >
+              Home
             </Link>
           </li>
           <li>
-            <Link to="/register" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+            <Link
+              to="/register"
+              className="text-gray-300 hover:text-white transition duration-300 ease-in-out"
+            >
               Register
             </Link>
           </li>
           <li>
-            <Link to="/login" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+            <Link
+              to="/login"
+              className="text-gray-300 hover:text-white transition duration-300 ease-in-out"
+            >
               Login
             </Link>
           </li>
+          <li className="text-red-400">{user?.email}</li>
+          {user && (
+            <li>
+              <button
+             
+                className="text-gray-300 hover:text-white transition duration-300 ease-in-out"
+                onClick={handleLogOut}
+              >
+                Sign Out
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
